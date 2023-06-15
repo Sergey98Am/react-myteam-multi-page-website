259be5fe2ba6a40c659f93e768482efb1b2ff7dd
Serg <sergey.gabrielyan1998@gmail.com>
2023-06-15 19:51:31 +0400

    Add breakpoints in feedback card

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,11 +1,11 @@
 const FeedbackCard = (props) => {
   return (
     <div className="feedback-card flex items-center flex-col text-center pt-9">
-      <div className="feedback-description mb-8">
+      <div className="feedback-description mb-4 lg:mb-8">
         <p className="body-2">{props.description}</p>
       </div>
       <div className="feedback-user">
-        <div className="user-info mb-8">
+        <div className="user-info mb-4 lg:mb-8">
           <h3 className="feedback-user-name text-secondary-rapture-blue">
             {props.user_name}
           </h3>
