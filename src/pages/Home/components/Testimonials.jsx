5f1ba0539f726d3c7eacd8938109a335d33c76bb
Serg <sergey.gabrielyan1998@gmail.com>
2023-06-15 18:20:37 +0400

    Change testimonials title width

--- src/pages/Home/components/Testimonials.jsx
+++ src/pages/Home/components/Testimonials.jsx
@@ -2,7 +2,7 @@ const Testimonials = () => {
   return (
     <div className='testimonials small-screens-section bg-primary-midnight-green py-36 md:py-24 lg:py-36'>
       <div className="container-fluid">
-        <div className="testimonials-title text-center mx-auto md:max-w-[30rem] lg:max-w-5xl">
+        <div className="testimonials-title text-center mx-auto md:max-w-[30rem] lg:max-w-[58.5rem]">
           <h2>Delivering real results for top companies. Some of our <span className="text-secondary-rapture-blue">success stories.</span></h2>
         </div>
       </div>
