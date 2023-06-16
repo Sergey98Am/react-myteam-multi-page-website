b14f970ac7b6d4dc7a88b2ccc370f6ef33cf761b
Serg <sergey.gabrielyan1998@gmail.com>
2023-06-16 19:21:41 +0400

    Add footer section

---
+++ src/components/SocialIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SocialIcon = (props) => {
+  return (
+    <a href={props.link} target="blank"><img src={props.icon} alt="facebook" /></a>
+  )
+}
+
+export default SocialIcon
