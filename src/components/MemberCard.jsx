f42cbd8cdceb09b7ce63bc185fef3ff552afd5f3
Serg <sergey.gabrielyan1998@gmail.com>
2023-06-23 19:05:09 +0400

    Change preserve-3d  member card for other devices work

--- src/components/MemberCard.jsx
+++ src/components/MemberCard.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import iconCross from "@/assets/icon-cross.svg";
-import avatarNikita from "@/assets/avatar-nikita.jpg";
 import ctl from "@netlify/classnames-template-literals";
 import SocialIcon from "./SocialIcon";
 
@@ -13,36 +12,33 @@ const MemberCard = (props) => {
     h-[256px] 
     bg-transparent  
     text-center
+    relative
   `);
 
   const frontClasses = ctl(`
     front 
     absolute 
-    backface-hidden 
     w-full 
     h-full 
     bg-secondary-sacramento-state-green 
     px-12 
     pt-8 
     pb-14
-    z-20
     member-front
-    ${flip && 'active'}
+    ${flip && "member-card-rotate"}
   `);
 
   const backClasses = ctl(`
     back 
     absolute 
-    backface-hidden 
     w-full 
     h-full 
     bg-secondary-dark-green 
     px-12 
     pt-8 
     pb-14
-    z-10
     member-back
-    ${flip && 'active'}
+    ${flip && "member-card-rotate"}
   `);
 
   const buttonClasses = ctl(`
@@ -69,55 +65,49 @@ const MemberCard = (props) => {
 
   return (
     <div className={cardClasses}>
-      <div
-        className={`relative card-content w-full h-full duration-1000`}
-      >
-        <div className={frontClasses}>
-          <div className={`front-content ${cardContentClasses}`}>
-            <div className="user-img mb-4 mx-auto">
-              <img
-                className="w-24 h-24 rounded-full border-2 border-secondary-rapture-blue"
-                src={props.data.image}
-                alt="avatar-nikita"
-              />
-            </div>
-            <div className="front-member-info">
-              <h3 className="member-name text-secondary-rapture-blue">
-                {props.data.name}
-              </h3>
-              <h4 className="member-profession text-xs1 italic">
-                {props.data.profession}
-              </h4>
-            </div>
+      <div className={frontClasses}>
+        <div className={`front-content ${cardContentClasses}`}>
+          <div className="user-img mb-4 mx-auto">
+            <img
+              className="w-24 h-24 rounded-full border-2 border-secondary-rapture-blue"
+              src={props.data.image}
+              alt="avatar-nikita"
+            />
           </div>
-          <button
-            className={`bg-primary-light-coral ${buttonClasses} transition hover:bg-secondary-rapture-blue`}
-            onClick={() => setFlip(!flip)}
-          >
-            <img src={iconCross} alt="icon-cross" />
-          </button>
-        </div>
-        <div className={backClasses}>
-          <div className={`back-content ${cardContentClasses}`}>
-            <h3 className="member-name text-secondary-rapture-blue mb-2">
+          <div className="front-member-info">
+            <h3 className="member-name text-secondary-rapture-blue">
               {props.data.name}
             </h3>
-            <p className="member-text body-2 mb-6">
-              “{props.data.text}”
-            </p>
-            <div className={socialMediaClasses}>
-              {props.data.socialMedia.map((link) => (
-                <SocialIcon key={link.id} link={link.link} icon={link.icon} />
-              ))}
-            </div>
+            <h4 className="member-profession text-xs1 italic">
+              {props.data.profession}
+            </h4>
           </div>
-          <button
-            className={`bg-secondary-rapture-blue ${buttonClasses}`}
-            onClick={() => setFlip(!flip)}
-          >
-            <img className="rotate-45" src={iconCross} alt="icon-close" />
-          </button>
         </div>
+        <button
+          className={`bg-primary-light-coral ${buttonClasses} transition hover:bg-secondary-rapture-blue`}
+          onClick={() => setFlip(!flip)}
+        >
+          <img src={iconCross} alt="icon-cross" />
+        </button>
+      </div>
+      <div className={backClasses}>
+        <div className={`back-content ${cardContentClasses}`}>
+          <h3 className="member-name text-secondary-rapture-blue mb-2">
+            {props.data.name}
+          </h3>
+          <p className="member-text body-2 mb-6">“{props.data.text}”</p>
+          <div className={socialMediaClasses}>
+            {props.data.socialMedia.map((link) => (
+              <SocialIcon key={link.id} link={link.link} icon={link.icon} />
+            ))}
+          </div>
+        </div>
+        <button
+          className={`bg-secondary-rapture-blue ${buttonClasses}`}
+          onClick={() => setFlip(!flip)}
+        >
+          <img className="rotate-45" src={iconCross} alt="icon-close" />
+        </button>
       </div>
     </div>
   );
